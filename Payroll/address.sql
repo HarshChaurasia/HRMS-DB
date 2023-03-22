08b7f64087ad0d1545b1d839b3493d18fbc3943e
sourabhtikone <sourabhtikone5864@gmail.com>
2023-03-22 18:42:36 +0530

    second commit

---
+++ Payroll/address.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[address]
+(
+	[emp_id] INT NOT NULL PRIMARY KEY, 
+    [address_line1] VARCHAR(50) NOT NULL, 
+    [asddress_line2] VARCHAR(50) NULL, 
+    [country] VARCHAR(50) NOT NULL, 
+    [state] VARCHAR(50) NOT NULL, 
+    [city] VARCHAR(50) NOT NULL, 
+    [is_current] VARCHAR(50) NOT NULL
+)
---
+++ Payroll/deduction.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[deduction]
+(
+	[trans__Id] INT NOT NULL PRIMARY KEY, 
+    [emp_id] INT NOT NULL, 
+    [emp_da] VARCHAR(50) NULL, 
+    [emp_gross] VARCHAR(50) NOT NULL, 
+    [emp_basic] VARCHAR(50) NOT NULL, 
+    [emp_pf] VARCHAR(50) NOT NULL, 
+    [emp_pt] VARCHAR(50) NOT NULL, 
+    [emp_tds] VARCHAR(50) NULL, 
+    [emp_esi] VARCHAR(50) NULL, 
+    [taxrequest] VARCHAR(50) NOT NULL
+)
