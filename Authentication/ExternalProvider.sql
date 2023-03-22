d18880f5271695c837be533ce78649685278d974
Madhavi Lad <madhavilad08@gmail.com>
2023-03-22 17:58:29 +0530

    Adding Authentication Tables

---
+++ Authentication/ExternalProvider.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ExternalProvider]
+(
+	[ExternalProviderID] INT NOT NULL  IDENTITY, 
+    [ProviderName] NVARCHAR(50) NULL, 
+    [WSEndPoint] NVARCHAR(50) NULL, 
+    CONSTRAINT [PK_ExternalProvider] PRIMARY KEY ([ExternalProviderID])
+)
