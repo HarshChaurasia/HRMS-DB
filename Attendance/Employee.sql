4eef0e81538353bbc8676f4d84aec089ae8e99aa
Adnan Attar <73285966+adnanattar@users.noreply.github.com>
2023-03-22 18:44:41 +0530

    Adding Attendance Tables
    Address
    AddressType
    Attendance
    AttendanceInfo
    Employee
    EmployeeAddress
    
    Added By Adnan Attar

---
+++ Attendance/Employee.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[FirstName] NVARCHAR(50) NOT NULL , 
+    [MiddlenName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [DOB] DATE NULL, 
+    [MobileNumber] NVARCHAR(10) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [EmployeeID] NVARCHAR(50) NOT NULL, 
+    [DepartmentID] NVARCHAR(50) NULL, 
+    PRIMARY KEY ([EmployeeID])
+)
