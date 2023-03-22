ad999c06abfde84bad00edf4d282f545cb043816
Priyanka Suryakant Nikam <arohiinfo2@gmail.com>
2023-03-22 18:22:08 +0530

    Adding new tables Authentication GrantedPermission Permission UserAccount UserloginExternal UserRole
    Added By Priyanka Nikam

---
+++ Authentication/UserAccount.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[UserLoginDataExternal]
+(
+	[UserID] INT NOT NULL  IDENTITY, 
+    [FirstName] NVARCHAR(200) NULL, 
+    [MiddleName] NVARCHAR(200) NULL, 
+    [LastName] NVARCHAR(200) NULL, 
+    [Gender] NVARCHAR(20) NULL, 
+    [DOB] DATE NULL, 
+    [MobileNumber] INT NULL, 
+    [Email] NVARCHAR(200) NULL, 
+    [RoleID] INT NULL, 
+    CONSTRAINT [PK_UserLoginDataExternal] PRIMARY KEY ([UserID])
+)
